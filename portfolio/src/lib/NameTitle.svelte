5e6e5ee378ab4f875379a5b987c3e97e6e36a109
RuxMgit <eliottmorisset85@gmail.com>
2026-02-19 10:56:09 +0100

    Initial Commit

---
+++ portfolio/src/lib/NameTitle.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+
+
+</script>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Almendra+Display&display=swap');
+
+    .almendra-display-regular {
+        font-family: "Almendra Display", serif;
+        font-weight: 400;
+        font-style: normal;
+    }
+</style>
+
+<h1 class="almendra-display-regular">Eliott Morisset</h1>
+
