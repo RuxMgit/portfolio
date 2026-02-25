73f468ed410b0a1c09a87e193303a730a2aa5df9
RuxMgit <eliottmorisset85@gmail.com>
2026-02-25 14:30:15 +0100

    Changed way of importing assets

--- src/lib/projects/Projects.svelte
+++ src/lib/projects/Projects.svelte
@@ -1,14 +1,7 @@
 <script>
     import ProjectCard from "./ProjectCard.svelte";
-    let skyjoCardData = $state(null);
-    let chuuiCardData = $state(null);
-
-    fetch('/projectsData/skyjoCard.json')
-        .then(r => r.json())
-        .then(data => skyjoCardData = data);
-    fetch('/projectsData/chuuiCard.json')
-        .then(r => r.json())
-        .then(data => chuuiCardData = data);
+    import skyjoCardData from "../../assets/projectsData/skyjoCard.json";
+    import chuuiCardData from "../../assets/projectsData/chuuiCard.json";
 
 
 </script>
