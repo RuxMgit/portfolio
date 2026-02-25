4b6af542d7641fdbd1509ba8ca12db90800c70f8
RuxMgit <eliottmorisset85@gmail.com>
2026-02-25 13:02:02 +0100

    2cnd card

---
+++ src/lib/projects/Projects.svelte
@@ -0,0 +1,20 @@
+<script>
+    import ProjectCard from "./ProjectCard.svelte";
+    let skyjoCardData = $state(null);
+    let chuuiCardData = $state(null);
+
+    fetch('/projectsData/skyjoCard.json')
+        .then(r => r.json())
+        .then(data => skyjoCardData = data);
+    fetch('/projectsData/chuuiCard.json')
+        .then(r => r.json())
+        .then(data => chuuiCardData = data);
+
+
+</script>
+
+
+{#if skyjoCardData && chuuiCardData}
+    <ProjectCard direction="leftToRight" data={skyjoCardData}/>
+    <ProjectCard direction="rightToLeft" data={chuuiCardData}/>
+{/if}
